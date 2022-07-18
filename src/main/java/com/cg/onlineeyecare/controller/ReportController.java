f65dc3e11631106035936b63ef63378af6803e63
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2022-07-18 13:05:50 +0530

    Update ReportController.java

--- src/main/java/com/cg/onlineeyecare/controller/ReportController.java
+++ src/main/java/com/cg/onlineeyecare/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.cg.onlineeyecare.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.onlineeyecare.entity.Spectacles;
+
 import com.cg.onlineeyecare.model.ReportDTO;
 import com.cg.onlineeyecare.service.ReportService;
 
@@ -39,4 +43,9 @@ public class ReportController {
 	public ReportDTO viewReport(@PathVariable("reportId") int reportId) {
 		return reportService.viewReport(reportId);
 	}
+	@GetMapping("/getspectacles/all")
+	public Iterable<Spectacles> getAllSpectacles(){
+		return reportService.viewSpetacles();
+	}
+	
 }
