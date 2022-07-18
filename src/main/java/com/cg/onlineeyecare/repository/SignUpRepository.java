367ccc94a52d39a31e9b82d28c235843e0167b65
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2022-07-18 16:24:51 +0530

    sign up repo

---
+++ src/main/java/com/cg/onlineeyecare/repository/SignUpRepository.java
@@ -0,0 +1,13 @@
+package com.cg.onlineeyecare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cg.onlineeyecare.entity.Signups;
+import com.google.common.base.Optional;
+
+@Repository
+public interface SignUpRepository extends JpaRepository<Signups, Integer>{
+
+	Optional<Signups>findByUsername(String username);
+
+}
