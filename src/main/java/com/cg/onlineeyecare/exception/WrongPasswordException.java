cc69613265a50d56bd3737eddbc4adcad26ee803
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2022-07-18 16:49:57 +0530

    password exception

---
+++ src/main/java/com/cg/onlineeyecare/exception/WrongPasswordException.java
@@ -0,0 +1,17 @@
+package com.cg.onlineeyecare.exception;
+
+public class WrongPasswordException extends RuntimeException {
+	private static final long serialVersionUID = -512056536889030695L;
+
+	public WrongPasswordException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public WrongPasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
